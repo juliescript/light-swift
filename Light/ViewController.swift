c42a0eece9c90dfd79d0a232e3f5f950aed3b896
Julieta Campos <julieicelie@gmail.com>
2020-09-07 22:55:49 +0200

    Finish light project

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -10,18 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     var lightOn = true
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
     fileprivate func updateUI() {
-        if lightOn {
-            view.backgroundColor = .white
-        } else {
-            view.backgroundColor = .black
-        }
+        view.backgroundColor = lightOn ? .white : .black
     }
     
     @IBAction func buttonPressed(_ sender: Any) {
